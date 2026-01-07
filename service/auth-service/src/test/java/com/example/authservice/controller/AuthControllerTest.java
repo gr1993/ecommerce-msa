b5dd21a5a8d26907952dfc5a85d1b9068d51d75f
hppro2024\smcore2024 <klpark@smcore.co.kr>
2026-01-07 14:09:04 +0900

    test(auth): docker 빌드 시 테스트 통과하도록 Config Server 비활성화 설정

--- service/auth-service/src/test/java/com/example/authservice/controller/AuthControllerTest.java
+++ service/auth-service/src/test/java/com/example/authservice/controller/AuthControllerTest.java
@@ -10,8 +10,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(AuthController.class)
+@ActiveProfiles("test")
 @DisplayName("AuthController 테스트")
 class AuthControllerTest {
 
@@ -32,7 +34,7 @@ class AuthControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private AuthService authService;
 
     @Test
