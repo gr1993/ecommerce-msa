bdb4899ab550524858ddd3a4b32f772f59a1a929
hppro2024\smcore2024 <klpark@smcore.co.kr>
2026-01-23 17:28:15 +0900

    feat(product): springwolf UI에 날짜 필드 발행 설정

--- service/product-service/src/main/java/com/example/productservice/product/domain/event/ProductCreatedEvent.java
+++ service/product-service/src/main/java/com/example/productservice/product/domain/event/ProductCreatedEvent.java
@@ -1,5 +1,7 @@
 package com.example.productservice.product.domain.event;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,5 +23,8 @@ public class ProductCreatedEvent {
 	private String status;
 	private Boolean isDisplayed;
 	private List<Long> categoryIds;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
+	@Schema(description = "생성 일시", example = "2026-01-23T16:58:34.035882", type = "string")
 	private LocalDateTime createdAt;
 }
