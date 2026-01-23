6d450a86c69b07788cfa3fe2adecbc427bc55eea
hppro2024\smcore2024 <klpark@smcore.co.kr>
2026-01-23 17:32:09 +0900

    feat(user): springwolf UI에 날짜 필드 발행 설정

--- service/user-service/src/main/java/com/example/userservice/domain/event/UserRegisteredEvent.java
+++ service/user-service/src/main/java/com/example/userservice/domain/event/UserRegisteredEvent.java
@@ -1,5 +1,7 @@
 package com.example.userservice.domain.event;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +18,9 @@ public class UserRegisteredEvent {
 	private String phone;
 	private String role;
 	private String hashedPassword;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
+	@Schema(description = "생성 일시", example = "2026-01-23T16:58:34.035882", type = "string")
 	private LocalDateTime registeredAt;
 }
 
