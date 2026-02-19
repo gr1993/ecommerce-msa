bfd0e39605a866b49089337ecb2b8ef1d4fd316c
hppro2024\smcore2024 <klpark@smcore.co.kr>
2026-02-19 17:09:23 +0900

    feat(infra): mock-delivery-server 초기 업로드

---
+++ infra/mock-delivery-server/src/main/java/com/example/mockdelivery/MockDeliveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockDeliveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockDeliveryServerApplication.class, args);
+	}
+
+}
---
+++ infra/mock-delivery-server/src/test/java/com/example/mockdelivery/MockDeliveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockDeliveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
